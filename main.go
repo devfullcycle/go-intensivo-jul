1ebdd5ff08fb456c3073e6e68de26d73145f5783
Wesley <wesleywillians@gmail.com>
2023-07-17 21:27:53 -0400

    adding defer

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	defer db.Close() // espera tudo rodar e depois executa o close
 	orderRepository := database.NewOrderRepository(db)
 	uc := usecase.NewCalculateFinalPrice(orderRepository)
 
